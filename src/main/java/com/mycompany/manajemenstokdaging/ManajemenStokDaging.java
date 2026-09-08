58d63abe2f1274bcc2639a7e840a053f8827b115
Aditya Dwinugraha Yusniandra <adityadwinugrahayusniandra@gmail.com>
2026-09-08 13:37:32 +0800

    Initial Commit

--- src/main/java/com/mycompany/manajemenstokdaging/ManajemenStokDaging.java
+++ src/main/java/com/mycompany/manajemenstokdaging/ManajemenStokDaging.java
@@ -11,6 +11,6 @@ package com.mycompany.manajemenstokdaging;
 public class ManajemenStokDaging {
 
     public static void main(String[] args) {
-        System.out.println("Halo Duniaaaaaa");
+        System.out.println("Halosss Duniaaaaaa");
     }
 }
