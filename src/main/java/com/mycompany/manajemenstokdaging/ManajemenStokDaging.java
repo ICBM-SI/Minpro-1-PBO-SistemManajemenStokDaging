d22abd2a55d2906e29720769efe65020c87150b7
Aditya Dwinugraha Yusniandra <adityadwinugrahayusniandra@gmail.com>
2026-09-08 13:25:05 +0800

    Initial Commit

---
+++ src/main/java/com/mycompany/manajemenstokdaging/ManajemenStokDaging.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manajemenstokdaging;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ManajemenStokDaging {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
